8fb468d09259862dde0ea72dfc0ecc732167fe54
fare-alkhadrawy <fars.alkhadrawy@student.hu.nl>
2021-03-24 03:02:28 +0100

    add service functions

--- src/main/java/nl/hu/cisq1/lingo/trainer/application/GameService.java
+++ src/main/java/nl/hu/cisq1/lingo/trainer/application/GameService.java
@@ -1,20 +1,45 @@
 package nl.hu.cisq1.lingo.trainer.application;
 
+
+
+import javassist.NotFoundException;
 import nl.hu.cisq1.lingo.trainer.data.SpringGameRepository;
+import nl.hu.cisq1.lingo.trainer.domain.Game;
 import nl.hu.cisq1.lingo.words.application.WordService;
 import org.springframework.stereotype.Service;
 
 import javax.transaction.Transactional;
 
-@Transactional
-@Service
-public class GameService {
-    private final WordService word;
-    private final SpringGameRepository repository;
 
-    public GameService(WordService word, SpringGameRepository repository){
-        this.repository = repository;
+@Service
+@Transactional
+public class GameService {
+    private WordService word;
+    private SpringGameRepository repository;
+
+    public GameService(WordService word, SpringGameRepository repository) {
         this.word = word;
+        this.repository = repository;
+    }
+
+    public Game startGame(){
+        return repository.save(new Game(word.provideRandomWord(5)));
+    }
+
+    public Game startNewRound(long id) throws NotFoundException {
+        Game game = repository.findById(id)
+                .orElseThrow(()-> new NotFoundException(String.format("game with id (%s) not founded",id)));
+        game.startRound(word.provideRandomWord(game.wordLength()));
+        repository.save(game);
+        return game;
+    }
+
+    public Game doGuess(String attempt, long id) throws NotFoundException {
+        Game game = repository.findById(id)
+                .orElseThrow(()-> new NotFoundException(String.format("game with id (%s) not founded",id)));;
+        game.guessWord(attempt);
+        repository.save(game);
+        return game;
     }
 
 }
