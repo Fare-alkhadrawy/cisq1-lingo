fe1663d68bf7bdc2788a2f2bb18b633d420bc73a
fare-alkhadrawy <fars.alkhadrawy@student.hu.nl>
2021-03-25 22:43:41 +0100

    add converters class

---
+++ src/main/java/nl/hu/cisq1/lingo/trainer/data/HintConverter.java
@@ -0,0 +1,27 @@
+package nl.hu.cisq1.lingo.trainer.data;
+
+import nl.hu.cisq1.lingo.trainer.domain.Mark;
+
+import javax.persistence.AttributeConverter;
+import javax.persistence.Convert;
+import java.util.Arrays;
+import java.util.List;
+import java.util.Objects;
+import java.util.stream.Collectors;
+
+public class HintConverter implements AttributeConverter<List<String>, String>{
+
+    @Override
+    public String convertToDatabaseColumn (List<String> strings) {
+        return strings.stream()
+                .map(Objects::toString)
+                .collect(Collectors.joining(","));
+    }
+
+    @Override
+    public List<String> convertToEntityAttribute (String s) {
+        return Arrays.stream(s.split(","))
+                .map(String::valueOf)
+                .collect(Collectors.toList());
+    }
+}
