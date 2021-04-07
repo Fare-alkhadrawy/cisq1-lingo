25fb0a2cf3480396ecffff2af4caae4780682855
fare-alkhadrawy <fars.alkhadrawy@student.hu.nl>
2021-04-07 16:24:38 +0200

    add controller integration test

--- src/main/java/nl/hu/cisq1/lingo/trainer/presentation/DTOs/GameDTO.java
+++ src/main/java/nl/hu/cisq1/lingo/trainer/presentation/DTOs/GameDTO.java
@@ -2,6 +2,7 @@ package nl.hu.cisq1.lingo.trainer.presentation.DTOs;
 
 import lombok.Data;
 import nl.hu.cisq1.lingo.trainer.domain.Game;
+import nl.hu.cisq1.lingo.trainer.domain.RoundStatus;
 
 import java.util.List;
 
@@ -9,11 +10,14 @@ import java.util.List;
 public class GameDTO {
     private int score;
     private int roundNumber;
+    private RoundStatus roundStatus;
     private List<String> hints;
 
     public GameDTO(Game game){
+
         this.score = game.getScore();
         this.roundNumber = game.roundNumber();
+        this.roundStatus = game.getLastRound().getRoundStatus();
         this.hints = game.getLastRound().getLastHint();
     }
 }
--- src/main/java/nl/hu/cisq1/lingo/trainer/presentation/GameController.java
+++ src/main/java/nl/hu/cisq1/lingo/trainer/presentation/GameController.java
@@ -3,9 +3,15 @@ package nl.hu.cisq1.lingo.trainer.presentation;
 import javassist.NotFoundException;
 import nl.hu.cisq1.lingo.trainer.application.GameService;
 import nl.hu.cisq1.lingo.trainer.domain.Game;
+import nl.hu.cisq1.lingo.trainer.exception.GameNotFoundException;
+import nl.hu.cisq1.lingo.trainer.exception.IllegalMoveException;
+import nl.hu.cisq1.lingo.trainer.exception.InvalidAttemptException;
+import nl.hu.cisq1.lingo.trainer.exception.RoundPlayingException;
 import nl.hu.cisq1.lingo.trainer.presentation.DTOs.GameDTO;
 import nl.hu.cisq1.lingo.trainer.presentation.DTOs.GuessDTO;
+import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.server.ResponseStatusException;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -28,16 +34,26 @@ public class GameController {
 
     @PostMapping("/guess")
 
-    public GameDTO guessWord(@RequestBody GuessDTO guessDTO) throws NotFoundException {
-        return new GameDTO(service.doGuess(guessDTO.guess, guessDTO.id));
+    public GameDTO guessWord(@RequestBody GuessDTO guessDTO){
+        try {
+            return new GameDTO(service.doGuess(guessDTO.guess, guessDTO.id));
+        }catch (InvalidAttemptException | IllegalMoveException e){
+            throw new ResponseStatusException(HttpStatus.BAD_REQUEST, e.getMessage());
+        }catch (GameNotFoundException e){
+            throw new ResponseStatusException(HttpStatus.NOT_FOUND, e.getMessage());
+        }
     }
     @PostMapping("/startRound/{id}")
 
-    public GameDTO startRound(@PathVariable long id) throws NotFoundException {
-        return new GameDTO(service.startNewRound(id));
+    public GameDTO startRound(@PathVariable long id){
+        try {
+            return new GameDTO(service.startNewRound(id));
+        }catch (RoundPlayingException | GameNotFoundException e){
+            throw new ResponseStatusException(HttpStatus.BAD_REQUEST, e.getMessage());
+        }
     }
-    @PostMapping("/test")
-    public Game guess(@RequestBody GuessDTO guessDTO) throws NotFoundException {
-        return service.doGuess(guessDTO.guess, guessDTO.id);
-    }
+//    @PostMapping("/test")
+//    public Game guess(@RequestBody GuessDTO guessDTO) throws NotFoundException {
+//        return service.doGuess(guessDTO.guess, guessDTO.id);
+//    }
 }
